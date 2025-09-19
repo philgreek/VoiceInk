99d12617641a71a365b67344c9d9ba58911a52f7
FILIPP EGOROV <148784537+philgreek@users.noreply.github.com>
2025-09-19 18:53:11 +0300

    "Progressive Web App (PWA)"

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'voiceink-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/index.tsx',
+  '/icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Serve from cache
+        }
+        return fetch(event.request); // Fetch from network
+      })
+  );
+});
